1df597bcab986e3b99469bb2c486c34062a485b3
Maximilian Walter <maximilian-walter@users.noreply.github.com>
2023-01-17 11:50:16 +0100

    [TASK] Send 404-error if secret is not found

--- src/Controller/ShowSecretController.php
+++ src/Controller/ShowSecretController.php
@@ -25,8 +25,12 @@ final class ShowSecretController extends AbstractController
     #[Route('/{id}', name: 'secrets:show', requirements: ['id' => '[A-Za-z0-9_-]+'], methods: ['GET'])]
     public function __invoke(string $id): Response
     {
-        return $this->render('secrets/show.html.twig', [
-            'secret' => $this->repository->find($id),
-        ]);
+        $secret = $this->repository->find($id);
+
+        return $this
+            ->render('secrets/show.html.twig', [
+                'secret' => $secret,
+            ])
+            ->setStatusCode($secret ? Response::HTTP_OK : Response::HTTP_NOT_FOUND);
     }
 }
