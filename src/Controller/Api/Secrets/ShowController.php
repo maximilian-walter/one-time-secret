30088a63cf605ed83666c5096b90054d155a4b6d
Maximilian Walter <maximilian-walter@users.noreply.github.com>
2023-07-11 18:33:54 +0200

    [FEATURE] Add access-log

--- src/Controller/Api/Secrets/ShowController.php
+++ src/Controller/Api/Secrets/ShowController.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace App\Controller\Api\Secrets;
 
 use App\Repository\SecretRepositoryInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,8 +22,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 final class ShowController extends AbstractController
 {
-    public function __construct(private readonly SecretRepositoryInterface $repository)
-    {
+    public function __construct(
+        private readonly SecretRepositoryInterface $repository,
+        private readonly LoggerInterface $accessLogger
+    ) {
     }
 
     #[Route('/api/secrets/{id}', name: 'api:secrets:show', requirements: ['id' => '[A-Za-z0-9_-]+'], methods: ['GET'])]
@@ -40,6 +43,12 @@ final class ShowController extends AbstractController
         }
 
         $this->repository->remove($entity);
+
+        $this->accessLogger->info(sprintf('The secret %s was accessed by %s', $id, $request->getClientIp()), [
+            'secret_id' => $id,
+            'client_ip' => $request->getClientIp(),
+            'user_agent' => $request->server->get('HTTP_USER_AGENT'),
+        ]);
 
         return $this->json($entity);
     }
